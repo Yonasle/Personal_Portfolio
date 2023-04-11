fbcad69a901863e1c686a7a789e4adc96b18e5be
ybadr99 <yousefbadr909@gmail.com>
2023-04-11 10:34:37 +0200

    Add pop-menu html and js

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ navLinks.forEach((link) => {
     mobileIcon.classList.remove('active');
   });
 });
+
+
+
+// Get the modal
+const modal = document.querySelector("#myModal");
+// Get the button that opens the modal
+const btn = document.querySelector(".seepro");
+// Get the <span> element that closes the modal
+const close = document.querySelector(".close");
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <close> (x), close the modal
+close.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
