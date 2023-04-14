65233699efe65078943db846ab41ae27ca87a0cd
Yonas Lemma <lemmayonas@gmail.com>
2023-04-14 12:25:00 +0300

    Mobile popup

--- script.js
+++ script.js
@@ -1,171 +1,106 @@
-const mobileIcon = document.querySelector(".hamburg");
-const mobileMenu = document.querySelector(".navbar");
-const navLinks = document.querySelectorAll(".navbar a");
+const mobileIcon = document.querySelector('.hamburg');
+const mobileMenu = document.querySelector('.navbar');
+const navLinks = document.querySelectorAll('.navbar a');
 
-mobileIcon.addEventListener("click", () => {
-  mobileIcon.classList.toggle("active");
-  mobileMenu.classList.toggle("active");
+mobileIcon.addEventListener('click', () => {
+  mobileIcon.classList.toggle('active');
+  mobileMenu.classList.toggle('active');
 });
 
 navLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    mobileMenu.classList.remove("active");
-    mobileIcon.classList.remove("active");
+  link.addEventListener('click', () => {
+    mobileMenu.classList.remove('active');
+    mobileIcon.classList.remove('active');
   });
-});
-
-const projects = [
-  {
-    name: "Tonic",
-    canopy: "CANOPY",
-    backend: "Back End Dev",
-    year: "2015",
-    description:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    languages: ["HTML", "Ruby on Rails", "JavaScript", "CSS"],
-  },
-  {
-    name: "Multi-Post Stories",
-    canopy: "Facebook",
-    backend: "Full Stack Dev",
-    year: "2015",
-    description:
-      "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
-    languages: ["HTML", "Ruby on rails", "CSS", "JavaScript"],
-  },
-  {
-    name: "Facebook 360",
-    canopy: "CANOPY",
-    backend: "Back End Dev",
-    year: "2015",
-    description:
-      "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
-    languages: ["HTML", "Ruby on rails", "CSS", "JavaScript"],
-  },
-  {
-    name: "Uber Navigation",
-    canopy: "Uber",
-    backend: "Lead Developer",
-    year: "2018",
-    description:
-      "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.",
-    languages: ["HTML", "Ruby on rails", "CSS", "JavaScript"],
-  },
-];
-
-const projectCardsDiv = document.querySelector(".project-cards");
-
-// Add each project card to the project-cards div
-projects.forEach((project) => {
-  const cardDiv = document.createElement("div");
-  cardDiv.innerHTML = `
-    <div class="grid_desktop grid_card">
-      <h2>${project.name}</h2>
-      <ul class="dcan">
-        <li class="dcanopy">${project.canopy}</li>
-        <li class="dbackend">${project.backend}</li>
-        <li class="dtwenty">${project.year}</li>
-      </ul>
-      <p>${project.description}</p>
-      <ul class="dlang">
-        <li class="dhtml">${project.languages[0]}</li>
-        <li class="dcss">${project.languages[1]}</li>
-        <li class="djs">${project.languages[2]}</li>
-        <li class="djs">${project.languages[3]}</li>
-      </ul>
-      <button class="dseepro">See project</button>
-    </div>
-  `;
-  projectCardsDiv.appendChild(cardDiv);
 });
 
 const data = [
   {
-    imgSrc: "./Asset/nasir.svg",
-    title: "Tonic",
-    canopy: "CANOPY",
-    role: "Back End Dev",
-    year: "2015",
+    imgSrc: './Asset/nasir.svg',
+    title: 'Tonic',
+    canopy: 'CANOPY',
+    role: 'Back End Dev',
+    year: '2015',
     description:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    languages: ["HTML", "CSS", "JavaScript"],
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    languages: ['HTML', 'CSS', 'JavaScript'],
   },
   {
-    imgSrc: "./Asset/avail.svg",
-    title: "Multi-Post stories",
-    canopy: "CANOPY",
-    role: "Back End Dev",
-    year: "2015",
+    imgSrc: './Asset/avail.svg',
+    title: 'Multi-Post stories',
+    canopy: 'CANOPY',
+    role: 'Back End Dev',
+    year: '2015',
     description:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    languages: ["HTML", "CSS", "JavaScript"],
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    languages: ['HTML', 'CSS', 'JavaScript'],
   },
   {
-    imgSrc: "./Asset/oval.svg",
-    title: "Tonic",
-    canopy: "CANOPY",
-    role: "Back End Dev",
-    year: "2015",
+    imgSrc: './Asset/oval.svg',
+    title: 'Tonic',
+    canopy: 'CANOPY',
+    role: 'Back End Dev',
+    year: '2015',
     description:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    languages: ["HTML", "CSS", "JavaScript"],
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    languages: ['HTML', 'CSS', 'JavaScript'],
   },
   {
-    imgSrc: "./Asset/prof.svg",
-    title: "Multi-Post Stories",
-    canopy: "CANOPY",
-    role: "Back End Dev",
-    year: "2015",
+    imgSrc: './Asset/prof.svg',
+    title: 'Multi-Post Stories',
+    canopy: 'CANOPY',
+    role: 'Back End Dev',
+    year: '2015',
     description:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    languages: ["HTML", "CSS", "JavaScript"],
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    languages: ['HTML', 'CSS', 'JavaScript'],
   },
 ];
 
-const container = document.querySelector(".grid_container");
+const container = document.querySelector('.grid_container');
 
 data.forEach((item) => {
-  const gridItem = document.createElement("div");
-  gridItem.classList.add("grid_item");
+  const gridItem = document.createElement('div');
+  gridItem.classList.add('grid_item');
 
-  const img = document.createElement("img");
+  const img = document.createElement('img');
   img.src = item.imgSrc;
-  img.alt = "card image";
+  img.alt = 'card image';
   gridItem.appendChild(img);
 
-  const title = document.createElement("h2");
+  const title = document.createElement('h2');
   title.textContent = item.title;
   gridItem.appendChild(title);
 
-  const canList = document.createElement("ul");
-  canList.classList.add("can");
+  const canList = document.createElement('ul');
+  canList.classList.add('can');
 
-  const canopy = document.createElement("li");
-  canopy.classList.add("canopy");
+  const canopy = document.createElement('li');
+  canopy.classList.add('canopy');
   canopy.textContent = item.canopy;
   canList.appendChild(canopy);
 
-  const role = document.createElement("li");
-  role.classList.add("backend");
+  const role = document.createElement('li');
+  role.classList.add('backend');
   role.textContent = item.role;
   canList.appendChild(role);
 
-  const year = document.createElement("li");
-  year.classList.add("twenty");
+  const year = document.createElement('li');
+  year.classList.add('twenty');
   year.textContent = item.year;
   canList.appendChild(year);
 
   gridItem.appendChild(canList);
 
-  const description = document.createElement("p");
+  const description = document.createElement('p');
   description.textContent = item.description;
   gridItem.appendChild(description);
 
-  const langList = document.createElement("ul");
-  langList.classList.add("lang");
+  const langList = document.createElement('ul');
+  langList.classList.add('lang');
 
   item.languages.forEach((lang) => {
-    const langItem = document.createElement("li");
+    const langItem = document.createElement('li');
     langItem.classList.add(lang.toLowerCase());
     langItem.textContent = lang;
     langList.appendChild(langItem);
@@ -173,111 +108,117 @@ data.forEach((item) => {
 
   gridItem.appendChild(langList);
 
-  const button = document.createElement("button");
-  button.classList.add("seepro");
-  button.textContent = "See project";
+  const button = document.createElement('button');
+  button.classList.add('seepro');
+  button.textContent = 'See project';
   gridItem.appendChild(button);
 
   container.appendChild(gridItem);
 });
 const popy = [
   {
-    imgSrc: "./Asset/nasir.svg",
-    title: "Tonic",
-    canopy: "CANOPY",
-    role: "Back End Dev",
-    year: "2015",
+    imgSrc: './Asset/nasir.svg',
+    title: 'Tonic',
+    canopy: 'CANOPY',
+    role: 'Back End Dev',
+    year: '2015',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    languages: ["HTML", "CSS", "JavaScript"],
+    languages: ['HTML', 'CSS', 'JavaScript'],
   },
   {
-    imgSrc: "./Asset/avail.svg",
-    title: "Multi-Post stories",
-    canopy: "CANOPY",
-    role: "Back End Dev",
-    year: "2015",
+    imgSrc: './Asset/avail.svg',
+    title: 'Multi-Post stories',
+    canopy: 'CANOPY',
+    role: 'Back End Dev',
+    year: '2015',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    languages: ["HTML", "CSS", "JavaScript"],
+    languages: ['HTML', 'CSS', 'JavaScript'],
   },
   {
-    imgSrc: "./Asset/oval.svg",
-    title: "Tonic",
-    canopy: "CANOPY",
-    role: "Back End Dev",
-    year: "2015",
+    imgSrc: './Asset/oval.svg',
+    title: 'Tonic',
+    canopy: 'CANOPY',
+    role: 'Back End Dev',
+    year: '2015',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    languages: ["HTML", "CSS", "JavaScript"],
+    languages: ['HTML', 'CSS', 'JavaScript'],
   },
   {
-    imgSrc: "./Asset/prof.svg",
-    title: "Multi-Post Stories",
-    canopy: "CANOPY",
-    role: "Back End Dev",
-    year: "2015",
+    imgSrc: './Asset/prof.svg',
+    title: 'Multi-Post Stories',
+    canopy: 'CANOPY',
+    role: 'Back End Dev',
+    year: '2015',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    languages: ["HTML", "CSS", "JavaScript"],
+    languages: ['HTML', 'CSS', 'JavaScript'],
   },
 ];
 
-const popupContainer = document.createElement("div");
-popupContainer.classList.add("popup-container");
+const popupContainer = document.createElement('div');
+popupContainer.classList.add('popup-container');
 document.body.appendChild(popupContainer);
 
-const seeprobtns = document.querySelectorAll(".seepro");
-seeprobtns.forEach((btn, index) => {
-  btn.addEventListener("click", () => {
-    // Show the popup for the project at the corresponding index
-    const html = popup(index);
-    popupContainer.innerHTML = html;
-    popupContainer.style.display = "block";
+function popup(param) {
+  if (param < 0 || param >= popy.length) {
+    // Handle the out-of-bounds case here
+    return param;
+  }
 
-    const closeBtn = document.querySelector(".close");
-    closeBtn.addEventListener("click", () => {
-      popupContainer.style.display = "none";
+  const seeprobtns = document.querySelectorAll('.seepro');
+  seeprobtns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      const html = popup(index);
+      popupContainer.innerHTML = html;
+      popupContainer.style.display = 'block';
+
+      const closeBtn = popupContainer.querySelector('.close');
+      closeBtn.addEventListener('click', () => {
+        popupContainer.style.display = 'none';
+      });
     });
   });
-});
 
-popupContainer.addEventListener("click", (event) => {
-  if (event.target === popupContainer) {
-    popupContainer.style.display = "none";
-  }
-});
+  popupContainer.addEventListener('click', (event) => {
+    if (event.target === popupContainer) {
+      popupContainer.style.display = 'none';
+    }
+  });
 
-function popup(parram) {
-  if (parram < 0 || parram >= popy.length) {
-    // Handle the out-of-bounds case here
-    console.error("Invalid parameter: ", parram);
-    return;
-  }
-  const html = `<div class="pone"> <span class="close">&times;</span><h1>${
-    popy[parram].title
-  }</h1><ul class="po"><li><span class="canop">${
-    popy[parram].canopy
-  }</span> <span class="role">${
-    popy[parram].role
-  }</span>  <span class="year"> ${
-    popy[parram].year
-  }</span> </li></ul><img src="${popy[parram].imgSrc}"></img><p>${
-    popy[parram].description
-  }</p> <ul class="hcj">
-  ${popy[parram].languages
+  const html = `
+    <div class="pone">
+      <span class="close">&times;</span>
+      <h1>${popy[param].title}</h1>
+      <ul class="po">
+        <li>
+          <span class="canop">${popy[param].canopy}</span>
+          <span class="role">${popy[param].role}</span>
+          <span class="year">${popy[param].year}</span>
+        </li>
+      </ul>
+      <img src="${popy[param].imgSrc}">
+      </img>
+      <p>${popy[param].description}</p>
+      <ul class="hcj">
+        ${popy[param].languages
     .map((lang) => `<li class="pop_language">${lang}</li>`)
-    .join("")}
-</ul> <button class="popnewa">See live</button> <button class="popnewb">See source</button>
-</div> `;
+    .join('')}
+      </ul>
+      <button class="popnewa">See live</button>
+      <button class="popnewb">See source</button>
+    </div>
+  `;
 
   return html;
 }
 
-const seeprobtn = document.querySelectorAll(".seepro");
+const seeprobtn = document.querySelectorAll('.seepro');
 
-for (let i = 0; i < seeprobtn.length; i++) {
-  seeprobtn[i].addEventListener("click", () => {
+for (let i = 0; i < seeprobtn.length; i += 1) {
+  seeprobtn[i].addEventListener('click', () => {
     popup();
   });
 }
