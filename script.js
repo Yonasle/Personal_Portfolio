bf76f3680071af0c7af995c3a31a1b5d98867586
Yonas Lemma <lemmayonas@gmail.com>
2023-04-14 19:32:58 +0300

    Fix the Mobile popup

--- script.js
+++ script.js
@@ -162,68 +162,56 @@ const popupContainer = document.createElement("div");
 popupContainer.classList.add("popup-container");
 document.body.appendChild(popupContainer);
 
-function popup(param) {
-  if (param < 0 || param >= popy.length) {
-    // Handle the out-of-bounds case here
-    return;
+popupContainer.addEventListener("click", (event) => {
+  if (event.target === popupContainer) {
+    popupContainer.style.display = "none";
   }
+});
 
-  const seeprobtns = document.querySelectorAll(".seepro");
-  seeprobtns.forEach((btn, index) => {
-    btn.addEventListener("click", () => {
-      const html = popup(index);
-      popupContainer.innerHTML = html;
-      popupContainer.style.display = "block";
-
-      const closeBtn = popupContainer.querySelector(".close");
-      closeBtn.addEventListener("click", () => {
-        popupContainer.style.display = "none";
-      });
-    });
-  });
-
-  popupContainer.addEventListener("click", (event) => {
-    if (event.target === popupContainer) {
-      popupContainer.style.display = "none";
-    }
-  });
-
-  const html = `
-    <div class="pone">
-      <span class="close">&times;</span>
-      <h1>${popy[param].title}</h1>
-      <ul class="po">
-        <li>
-          <span class="canop">${popy[param].canopy}</span>
-          <span class="role">${popy[param].role}</span>
-          <span class="year">${popy[param].year}</span>
-        </li>
-      </ul>
-      <img src="${popy[param].imgSrc}">
-      </img>
-      <p>${popy[param].description}</p>
-      <ul class="hcj">
-        ${popy[param].languages
-          .map((lang) => `<li class="pop_language">${lang}</li>`)
-          .join("")}
-      </ul>
-      <button class="popnewa">See live</button>
-      <button class="popnewb">See source</button>
-    </div>
-  `;
+function popup(parram) {
+  if (parram < 0 || parram >= popy.length) {
+    // Handle the out-of-bounds case here
+    return null;
+  }
+  const html = `<div class="pone"> <span class="close">&times;</span><h1>${
+    popy[parram].title
+  }</h1><ul class="po"><li><span class="canop">${
+    popy[parram].canopy
+  }</span> <span class="role">${
+    popy[parram].role
+  }</span>  <span class="year"> ${
+    popy[parram].year
+  }</span> </li></ul><img src="${popy[parram].imgSrc}"></img><p>${
+    popy[parram].description
+  }</p> <ul class="hcj">
+  ${popy[parram].languages
+    .map((lang) => `<li class="pop_language">${lang}</li>`)
+    .join("")}
+</ul> <button class="popnewa">See live</button> <button class="popnewb">See source</button>
+</div> `;
 
   return html;
 }
-
+const seeprobtns = document.querySelectorAll(".seepro");
 seeprobtns.forEach((btn, index) => {
   btn.addEventListener("click", () => {
-    const html = generatePopupHTML(index);
+    // Show the popup for the project at the corresponding index
+    const html = popup(index);
     popupContainer.innerHTML = html;
     popupContainer.style.display = "block";
 
-    const closeBtn = popupContainer.querySelector(".close");
+    const closeBtn = document.querySelector(".close");
     closeBtn.addEventListener("click", () => {
       popupContainer.style.display = "none";
     });
   });
 });
+const seeprobtn = document.querySelectorAll(".seepro");
+
+for (let i = 0; i < seeprobtn.length; i += 1) {
+  seeprobtn[i].addEventListener("click", () => {
+    popup(0);
+  });
+}
+
+/*  */
